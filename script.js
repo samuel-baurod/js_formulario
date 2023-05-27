bab5c1919ed3f52bd7ab69249bdfd8bf7af58fe2
Samuel br <samuel.br96iv@gmail.com>
2023-05-27 11:11:33 -0600

    clase03 completada

--- script.js
+++ script.js
@@ -3,7 +3,25 @@ const btn = document.querySelector("[data-form-btn]");
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector("[data-list]");
+    // se crea la variable task con el elemento li de html
+    const task = document.createElement("li");
+    //se agrega la clase card a task 
+    task.classList.add("card");
+    //limpia el valor del input cuando se le de clic en agregar
+    input.value = "";
+    // template strings o backticks sirven para poner codigo html en js
+    const content = `<div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">´${value}</span>
+        </div>
+        <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML = content;
+
+    list.appendChild(task);
+    console.log(content);
+
 }
 
 console.log(btn);
